c317caa49e7b25e2055808cf09528ccf84ff8526
Matthew Stickler <sticklerm3@me.com>
2018-11-30 20:14:21 -0600

    edited to link 'airport' to usr/local/sbin

--- airlink.fish
+++ airlink.fish
@@ -1,5 +1,5 @@
 #!/usr/bin/env fish
 
-function airlink --description 'a fish function to link airport utility to /usr/local/bin'
-    sudo ln -s /System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport /usr/local/bin/airport
+function airlink --description 'a fish function to link airport utility to /usr/local/sbin'
+    sudo ln -s /System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport /usr/local/sbin/airport
 end
